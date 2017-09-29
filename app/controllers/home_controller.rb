21368ae09fb74c52d15480b2ef886c7a3e0f410c
NadimRai <rai_nadim@yahoo.com>
2017-09-30 00:27:38 +0800

    added Faker gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
+	before_action :comment_content, only: [:index, :team]
+  
+  def comment_content
+  	@name = "Menlo App"
+  end
+
   def index
   end
 
